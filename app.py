c502e42e5f3f652397099625b5210cb800ee36bf
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-04 11:11:59 -0300

    perguntas e respostas

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def perguntas():
             df_null = df_null[df_null['Quantidade'] > 0]
             df_null['Percentual'] = (df_null['Quantidade'] / df.shape[0]) * 100
             df_null['Percentual'] = df_null['Percentual'].astype(int)
-            st.dataframe(df_null, width=None)
+            st.dataframe(df_null, width=200)
 
     with st.expander('2. Esses dados já proveem algum tipo de insight? Quais?'):
         st.markdown("""
