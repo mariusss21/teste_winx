c39a7456f7c35112eb77ac75bd22a6dbbbb0ef93
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-04 12:30:23 -0300

    perguntas e respostas

--- app.py
+++ app.py
@@ -144,17 +144,17 @@ def dashboard(df: pd.DataFrame) -> None:
 
         emp_a, emp_b, emp_c, legenda = st.columns(4)
 
-        fig = go.Figure(data=[go.Histogram(x=df_empresa_a.value, histnorm='probability')])
+        fig = go.Figure(data=[go.Histogram(x=df_empresa_a.value)])
 
         fig.update_layout(barmode='stack',
-            width=440, 
+            #width=440, 
             height=250,
             margin=dict(b=5,	t=35,	l=0,	r=0),
-            title='Percentual das calibrações',
+            title='Empresa A',
             font=dict(size=15))
 
         fig.update_traces(textposition='inside', textfont_color='rgb(255,255,255)', textfont_size=20) #marker_color='rgb(55,83,109)',
-        st.write(fig)
+        emp_a.write(fig)
 
         fig = ff.create_distplot([df_empresa_a.value], ['A'], bin_size=[1])
         emp_a.plotly_chart(fig, use_container_width=True)
