82a9092343f9f26bae4dbff8971974c6b6714025
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-04 11:06:06 -0300

    perguntas e respostas

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ def perguntas():
         with col1:
             st.write('Dados faltantes por colunas:')
             df_null = pd.DataFrame()
-            df_null['Quantidade'] = df.isnull().sum().astype(int)
+            df_null['Quantidade'] = df.isnull().sum()
             df_null.dropna(axis=1, inplace=True)
-            df_null = df_null[df_null > 0]
+            df_null = df_null[df_null['Quantidade'] > 0]
             df_null['Percentual'] = 0
             #df_null.columns.values[0] = 'Quantidade'
             #df_null.rename(columns={0: 'Quantidade'}, inplace=True)
