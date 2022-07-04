d2c5c1894154de458d0270a79b8ba00f1f149eec
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-04 11:06:36 -0300

    perguntas e respostas

--- app.py
+++ app.py
@@ -59,12 +59,8 @@ def perguntas():
             st.write('Dados faltantes por colunas:')
             df_null = pd.DataFrame()
             df_null['Quantidade'] = df.isnull().sum()
-            df_null.dropna(axis=1, inplace=True)
             df_null = df_null[df_null['Quantidade'] > 0]
-            df_null['Percentual'] = 0
-            #df_null.columns.values[0] = 'Quantidade'
-            #df_null.rename(columns={0: 'Quantidade'}, inplace=True)
-            #df_null['Percentual'] = df_null['Quantidade'] / df.shape[0] * 100
+            df_null['Percentual'] = df_null['Quantidade'] / df.shape[0] * 100
             st.write(df_null)
 
     with st.expander('2. Esses dados já proveem algum tipo de insight? Quais?'):
