cc9bac16a1a5022fce691e1dd248c5de35024837
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-07-04 10:51:14 -0300

    perguntas e respostas

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def perguntas():
 
         with col1:
             st.write('Dados faltantes por colunas:')
-            st.write(df.isnull().sum())
+            st.dataframe(df.isnull().sum())
 
     with st.expander('2. Esses dados já proveem algum tipo de insight? Quais?'):
         st.markdown("""
